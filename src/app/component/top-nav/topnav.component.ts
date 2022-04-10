bd96cb89255b5533f9019d82d32f1c02964e1710
Eric Yu <ericyu049@gmail.com>
2022-04-09 23:48:39 -0400

    proxy

--- src/app/component/top-nav/topnav.component.ts
+++ src/app/component/top-nav/topnav.component.ts
@@ -8,11 +8,5 @@ import { Component, OnInit } from "@angular/core";
 export class TopnavComponent implements OnInit {
     ngOnInit(): void {
     }
-    checkLoggedin() {
-        if (document.cookie.indexOf('ai-amadeus.auth=') === -1) return false;
-        else return true;
-    }
-    logout() {
-        document.cookie = 'ai-amadeus.auth'+'=; Max-Age=-99999999;';
-    }
+   
 }
